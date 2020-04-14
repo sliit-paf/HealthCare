11f400dba45b4efbb58c179687db62d8e3d70474
dhanuja-prathapa <dhanujaprathapa@gmail.com>
2020-04-14 13:47:25 +0530

    getHospitals created

--- src/main/java/com/project/healthcare/controller/HospitalController.java
+++ src/main/java/com/project/healthcare/controller/HospitalController.java
@@ -2,31 +2,61 @@ package com.project.healthcare.controller;
 
 import com.project.healthcare.model.Hospital;
 import java.util.ArrayList;
+import java.util.List;
 
 public class HospitalController implements IHospitalController{
+    List<Hospital> hospitals;
+
+    public HospitalController(){
+        hospitals = new ArrayList<>();
+        Hospital h1 = new Hospital();
+        h1.setId(1);
+        h1.setName("Hemas");
+        h1.setType("Eye");
+        h1.setDescription("Eye Hospital");
+        h1.setAddress("14/B");
+        h1.setPhone("078834893");
+
+        Hospital h2 = new Hospital();
+        h2.setId(2);
+        h2.setName("Cemas");
+        h2.setType("Eye");
+        h2.setDescription("Eye Hospital");
+        h2.setAddress("14/C");
+        h2.setPhone("078833893");
+
+        hospitals.add(h1);
+        hospitals.add(h2);
+    }
 
     @Override
-    public String createHospital(Hospital h) {
+    public List<Hospital> getHospitals(){
+        return hospitals;
+    }
+
+    @Override
+    public void createHospital(Hospital h) {
+        hospitals.add(h);
+    }
+
+    @Override
+    public Hospital getHospital(int id) {
+        Hospital h1 = null;
+        for (Hospital h : hospitals){
+            if(h.getId()==id)
+                return h;
+        }
+        return null;
+    }
+
+
+    @Override
+    public Hospital updateHospital(int id, Hospital new_id) {
         return null;
     }
 
     @Override
-    public Hospital getHospital(String hospitalId) {
-        return null;
-    }
-
-    @Override
-    public ArrayList<Hospital> getHospitals() {
-        return null;
-    }
-
-    @Override
-    public Hospital updateHospital(String hospitalId, Hospital id) {
-        return null;
-    }
-
-    @Override
-    public String deleteHospital(String hospitalId) {
+    public String deleteHospital(int id) {
         return null;
     }
 }
--- src/main/java/com/project/healthcare/resources/HospitalResource.java
+++ src/main/java/com/project/healthcare/resources/HospitalResource.java
@@ -1,46 +1,33 @@
 package com.project.healthcare.resources;
 
+import com.project.healthcare.controller.HospitalController;
 import com.project.healthcare.model.Hospital;
 
-import javax.validation.constraints.Max;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
-import java.util.Arrays;
 import java.util.List;
 
-@Path("hospital")
+@Path("hospitals")
 public class HospitalResource {
 
+    HospitalController repo = new HospitalController();
+
     @GET
-    @Path("id")
     @Produces(MediaType.APPLICATION_XML)
     public List<Hospital> getHospital() {
         System.out.println("getHospital called");
-        Hospital h1 = new Hospital();
-        h1.setId(1);
-        h1.setName("Hemas");
-        h1.setType("Eye");
-        h1.setDescription("Eye Hospital");
-        h1.setAddress("14/B");
-        h1.setPhone("078834893");
-
-        Hospital h2 = new Hospital();
-        h2.setId(2);
-        h2.setName("Cemas");
-        h2.setType("Eye");
-        h2.setDescription("Eye Hospital");
-        h2.setAddress("14/C");
-        h2.setPhone("078833893");
-
-        List<Hospital> hospitals = Arrays.asList(h1,h2);
-        return hospitals;
+        return repo.getHospitals();
     }
 
-    @GET
-    @Produces(MediaType.TEXT_PLAIN)
-    public String getIt() {
-        return "API works Yey!";
+    @POST
+    @Path("hospital")
+    public Hospital createHospital(Hospital h){
+        System.out.println(h);
+        repo.createHospital(h);
+        return h;
     }
+
 }
